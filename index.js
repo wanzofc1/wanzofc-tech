fcbcc09c80faae4f269b03d0fc5e7b450b3035f4
siputzx <159487561+siputzx@users.noreply.github.com>
2024-04-13 13:27:30 +0700

    Update index.js

--- index.js
+++ index.js
@@ -113,34 +113,37 @@ app.get('/api/blackboxAIChat', async (req, res) => {
 });
 
 app.get("/api/gpt", async (req, res) => {
-  const text = req.query.text;
+const text = req.query.text;
 
-  if (!text) {
-    return res.status(400).send("Parameter 'text' is required.");
-  }
+if (!text) {
+return res.status(400).send("Parameter 'text' is required.");
+}
 
-  try {
-    const { data } = await axios.post("https://api.rytr.me/", {
-      operation: "chatExecute",
-      params: {
-        text,
-        languageId: "6094f9b4addddd000c04c94b",
-        toneId: "60572a649bdd4272b8fe358c",
-        voiceId: ""
-      }
-    }, {
-      headers: {
-        Accept: "application/json, text/plain, */*",
-        Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MTZjMjFhMGE1NTNiNjE1MDhmNWIxOSIsImlhdCI6MTcxMjc2NzUxNH0.qseE0iNl-4bZrpQoB-zxVsc-pz13l3JOKkg4u6Y08OY",
-        "Content-Type": "application/json"
-      }
-    });
-    data.data.content = data.data.content.replace(/<\/?p[^>]*>/g, '');
-    res.json(data);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Internal Server Error");
-  }
+try {
+const requestData = {
+operation: "chatExecute",
+params: {
+text: text,
+languageId: "6094f9b4addddd000c04c94b",
+toneId: "60572a649bdd4272b8fe358c",
+voiceId: ""
+}
+};
+
+const config = {
+headers: {
+Accept: "application/json, text/plain, */*",
+Authentication: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY2MTZjMjFhMGE1NTNiNjE1MDhmNWIxOSIsImlhdCI6MTcxMjc2NzUxNH0.qseE0iNl-4bZrpQoB-zxVsc-pz13l3JOKkg4u6Y08OY",
+"Content-Type": "application/json"
+}
+};
+let {data} = await axios.post("https://api.rytr.me/", requestData, config)
+data.data.content = data.data.content.replace(/<\/?p[^>]*>/g, '');
+res.json(data);
+} catch (error) {
+console.error(error);
+res.status(500).send("Internal Server Error");
+}
 });
 
 
